7e01493169ecb06901e9158123289d5f2c8385ac
btokraks22 <bree.v.v.13@gmail.com>
2018-06-22 18:07:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,43 @@
 # dog.rb
+
+class Dog 
+  
+def initialize(name, breed)
+  @name = name
+  @breed = breed
+
+end
+
+def name
+  @name
+end
+
+def breed
+  @breed
+end
+
+dottie = Dog.new("Dottie", "Chug(Chiuaha)")
+puts "This dog's name is #{name}. #{name} is a #{breed}"
+
+end
+
+class Person
+  
+  def initialize(person_name, job)
+    @person_name = person_name
+    @job = job
+    
+  end
+  
+  def person_name
+    @person_name
+  end
+  
+  def job
+    @job
+  end
+  
+  melissa = Person.new("Melissa", "Statefarm agent")
+  puts "This person's name is #{person_name}. #{name} is a #{job}. #{name} owns the dog named #{name}."
+  
+end 
